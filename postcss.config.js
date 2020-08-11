7ed8af7446f2972b9b1f9529a63b1910977f70fb
Alexander Popov <alex.wayfer@gmail.com>
2020-08-11 17:50:40 +0300

    Add transitions for theme switching

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,6 @@
 module.exports = {
 	plugins: [
+		require('postcss-mixins'),
 		require('postcss-nested'),
 		require('autoprefixer'),
 		require('postcss-at-rules-variables')({ atRules: ['media'] })

65832f4bc563853e869f95f49f0772ab114d8c2b
Alexander Popov <alex.wayfer@gmail.com>
2020-08-14 15:08:26 +0300

    Improve scripts for dark mode
    Take out script for setting initial theme from `main.js` to `<script>`
    to prevent page blinking with white, especially in Firefox
    and with slow internet connection.
    
    Also set classes to `<html>` instead of `<body>`.

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -5,18 +5,11 @@ document.addEventListener('DOMContentLoaded', () => {
 		span.innerText = differenceInYears(Date.now(), window.birthday)
 	})
 
-	const osDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches
-
-	const savedDarkMode = localStorage.getItem('dark-theme')
-	if (savedDarkMode == 'true' || (savedDarkMode === undefined && osDarkMode)) {
-		document.body.classList.add('dark-theme')
-	}
-
 	document.querySelectorAll('button.theme-toggle').forEach(button => {
 		button.addEventListener('click', () => {
-			document.body.classList.toggle('dark-theme')
+			document.documentElement.classList.toggle('dark-theme')
 
-			localStorage.setItem('dark-theme', document.body.classList.contains('dark-theme'))
+			localStorage.setItem('dark-theme', document.documentElement.classList.contains('dark-theme'))
 		})
 	})
 

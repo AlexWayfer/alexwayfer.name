5424ffc8cb0c7ce19861af88f2b1bc441eb83549
Alexander Popov <alex.wayfer@gmail.com>
2022-11-29 23:02:52 +0300

    Remake `Data` from `OpenStruct` to `Hash`
    Resolve RuboCop offense.

--- exe/compile/data.rb
+++ exe/compile/data.rb
@@ -4,32 +4,19 @@ require 'ostruct'
 require 'yaml'
 
 module Compile
-	## Load data from YAML files and make `OpenStruct` object (for a prettier code)
-	class Data < OpenStruct
+	## Load data from YAML files
+	class Data < Hash
 		def initialize(yaml_files_directory)
-			super(
-				Dir.glob("#{yaml_files_directory}/**/*.y{a,}ml").each_with_object({}) do |file_name, result|
-					keys = file_name.match(%r{^#{yaml_files_directory}/(.+)\.ya?ml$})[1].split('/')
-					end_point_result = keys[..-2].reduce(result) do |nested_result, key|
-						nested_result[key] ||= initialize_open_struct_deeply({})
-					end
-					end_point_result[keys.last] = initialize_open_struct_deeply YAML.load_file file_name
+			super()
+
+			Dir.glob("#{yaml_files_directory}/**/*.y{a,}ml") do |file_name|
+				keys = file_name.match(%r{^#{yaml_files_directory}/(.+)\.ya?ml$})[1].split('/')
+
+				end_point_result = keys[..-2].reduce(self) do |nested_result, key|
+					nested_result[key] ||= {}
 				end
-			)
-		end
 
-		private
-
-		def initialize_open_struct_deeply(value)
-			case value
-			when Hash
-				self.class.superclass.new(
-					value.transform_values { |hash_value| send __method__, hash_value }
-				)
-			when Array
-				value.map { |element| send __method__, element }
-			else
-				value
+				end_point_result[keys.last] = YAML.load_file file_name
 			end
 		end
 	end
